854819bee99fb71ae3bc944fe3ff1ef1622cc362
Александр Яковлев <sanyayakovlev95@gmail.com>
2023-08-29 18:15:36 +0300

    added adaptive WeatherInfo

--- src/components/WeatherUpdates/index.jsx
+++ src/components/WeatherUpdates/index.jsx
@@ -5,18 +5,26 @@ import 'swiper/scss';
 import WeatherShapshot from '@components/WeatherShapshot';
 
 const WeatherUpdates = ({ hourlyForecast, scale }) => {
+  const parameters = {
+    spaceBetween: 50,
+    direction: 'horizontal',
+    mousewheel: true,
+    modules: [Mousewheel],
+    breakpoints: {
+      320: {
+        slidesPerView: 3,
+      },
+      560: {
+        slidesPerView: 4,
+      },
+    },
+  };
   return (
     <div className={styles.updates}>
-      <Swiper
-        spaceBetween={50}
-        slidesPerView={4}
-        direction={'horizontal'}
-        mousewheel={true}
-        modules={[Mousewheel]}
-      >
+      <Swiper {...parameters}>
         {hourlyForecast.map((el, index) => {
           return (
-            <SwiperSlide key={index}>
+            <SwiperSlide key={index} className={styles.slide}>
               <WeatherShapshot {...el} scale={scale} />
             </SwiperSlide>
           );
